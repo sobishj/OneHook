5f2f9db8d29cef70b98a3abb1f48d905be9d06b7
Sobish Joseph <sobishj@outlook.com>
2026-08-18 16:03:23 +0530

    Update go home label to just home

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // SprintGames PWA Service Worker
-const CACHE_NAME = 'sprintgames-shell-v5';
+const CACHE_NAME = 'sprintgames-shell-v6';
 
 // Static application shell assets
 const STATIC_ASSETS = [
